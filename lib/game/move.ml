783062e3b5e0713b6cbff4ddf2b0d0b18ceb0caf
Jason Zheng <jasonz4200@gmail.com>
2023-05-12 18:32:54 +0000

    Add documentation for measurement

--- lib/game/move.ml
+++ lib/game/move.ml
@@ -53,7 +53,7 @@ let measure (events : ('k * float) list) : 'k =
   let random = float_of_int (Random.int 100) in
   let filtered =
     List.filter
-      (fun (e, start, finish) -> random >= start && random < finish)
+      (fun (e, start, finish) -> start <= random && random < finish)
       buckets
   in
   match filtered with
@@ -211,27 +211,46 @@ let rec measure_piece (board : Board.t) (piece : quantum_piece) : Board.t =
      remaining superpositions. Recursively measure those that got kicked off *)
   board''
 
-(** [measurement board piece square] is the board after measurement occurs on
-    [square]. Measurement checks to see whether the piece on [square] actually
-    exists or not, and afterwards, it updates the board accordingly. *)
-let measurement (board : Board.t) (piece : quantum_piece) (square : coord) :
-    Board.t =
-  (* [update_board board piece square] updates [board] assuming that [piece] is
-     measured to be on [square] and nowhere else. All other probabilities are
-     deleted *)
-  match coord_checker board square with
-  | Empty | Stable -> board
-  | Unstable -> (
-      match tile with
-      | [] -> failwith "Error"
-      | piece :: t ->
-          let events =
-            List.map
-              (fun x -> ((x.file, x.rank), x.probability))
-              piece.superpositions
-          in
-          let true_coord = measure events in
-          update_board board piece square)
+(** [measurement board square] is the board after measurement occurs on
+    [square]. We perform measurement as follows:
+
+    Definition: The stability of a tile corresponds to the total percentage
+    present on that tile. We say that a piece is stable if the probabilities on
+    a tile add up to 100%. We say that a tile is unstable if the probabilities
+    add up to below 100%. We say that a tile is super-stable if the probability
+    exceeds 100%.
+
+    1. Find the piece that actually exists on [square] out of all
+    superpositions. There are two possible cases:
+
+    - First, there is a piece that has 100% probability. Then, we immediately
+      say this piece is the piece that exists on the board.
+    - Second, no pieces have 100% probability. Then, we choose a random number
+      to determine which piece actually exists.
+
+    2. Delete all other probabilities
+
+    - Delete the piece from all tiles except the tile corresponding to [square]
+    - Delete all superpositions of the piece except for [square]
+    - Delete all unused probability credits in the bank account
+
+    3. For all other superposition pieces on [square]
+
+    - Take the piece's probability p and add it to a bank account. The bank
+      account holds probability credits, which is probability that does not
+      appear on the board.
+    - From the bank account, while there still exists probability credits for
+      the piece, attempt to evenly spread all probability credits in the account
+      to all remaining superpositions of the piece. An attempt has three
+      possible outcomes:
+    - First, the attempt succeeds if the stability of the tile does not exceed
+      100%.
+    - Second, the attempt succeeds if adding the probability credits causes a
+      piece's probability on that tile to equal 100%.
+    - Third, the attempt is blocked if the tile becomes super-stable without any
+      specific piece to have a probability equal to 100%. In this case, we
+      measure the tile immediately and end this iteration of the while loop. *)
+let measurement (board : Board.t) (square : coord) : Board.t = board
 
 (** [capture_attempt phrase] is whether the player's move phrase is an attempt
     to capture an enemy piece *)
