190aa67bef34b5730dd7979660fe2b38d5a2169a
Alex Ni <awn33@cornell.edu>
2023-05-12 15:53:00 -0400

    command.parse completed

--- lib/game/command.mli
+++ lib/game/command.mli
@@ -1,27 +1,25 @@
 open State
 
-exception Malformed of string
-exception Empty of string
+exception Malformed
+exception Empty
 
 type move_phrase = {
   id : int;
-  start_tiles : tile option * tile option;
-  end_tiles : tile option * tile option;
+  start_tiles : coord option * coord option;
+  end_tiles : coord option * coord option;
 }
 (** The phrase representing a move, split, or merge *)
 
 type command =
   | Move of move_phrase
-  | Help
-  | Observe
-  | Resign
-  | Quit  (** The type representing a command issued to the board *)
+  | Draw
+  | Resign (** The type representing a command issued to the board *)
 
 val parse : string -> command
 (** [parse s] parses [s] into a command type. The input syntax consists of
     "<start_tile> <index> <end_tile>". Each element must be deliminated by at
     least 1 space.
-
+ 
     - <start_tile> and <end_tile> must be 2 characters. The first character
       should be a lowercase letter in \[a, h\]. The second character should be a
       number from \[1, 8\]
--- test/main.ml
+++ test/main.ml
@@ -1,5 +1,8 @@
 open OUnit2
 open Boardtest
 
-let suite = "test suite for Quantum Chess" >::: List.flatten [ Boardtest.tests ]
+let suite =
+  "test suite for Quantum Chess"
+  >::: List.flatten [ Boardtest.tests; Parsetest.tests ]
+
 let _ = run_test_tt_main suite
